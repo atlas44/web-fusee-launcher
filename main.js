be71e051d26a62baa228855bc795aac3ae333d6b
Elijah Bansley <elijahbansley@monroe.k12.wi.us>
2018-05-19 01:20:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ function readFileAsArrayBuffer(file) {
 }
 
 function logOutput(...message) {
-  document.getElementById("output").innerHTML += message.join(" ") + "\n";
+  document.getElementById("output").innerHTML = document.getElementById("output").innerHTML + message.join(" ") + "\n";
 }
 
 function clearLog() {
