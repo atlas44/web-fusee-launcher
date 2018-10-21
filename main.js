b40769d97008b14afa827e81ba389b388dfc2857
Elijah Bansley <elijahbansley@monroe.k12.wi.us>
2018-10-21 13:47:14 -0500

    debugginngggggg

--- main.js
+++ main.js
@@ -148,11 +148,15 @@ document.getElementById("goButton").addEventListener("click", async () => {
   let payload;
   if (payloadType === "CTCaer_Hekate") {
 var xhr = new XMLHttpRequest(); 
-xhr.open("GET", "https://github.com/ElijahZAwesome/web-cfw-loader/raw/master/hekate_ctcaer_4.2.bin"); 
+xhr.open("GET", "http://github.com/ElijahZAwesome/web-cfw-loader/raw/master/hekate_ctcaer_4.2.bin"); 
 xhr.responseType = "blob";//force the HTTP response, response-type header to be blob
 xhr.onload = function() 
 {
     payload = xhr.response;//xhr.response is now a blob object
+if(payload == null) {
+  console.log("is null oof");
+}
+console.log(payload);
 }
 xhr.send();
     //payload = CTCaer_Hekate;
